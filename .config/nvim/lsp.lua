d6d0abb12d5284a1a0dbe5cf7f73d3073af8f91c
Nick Pezza <pezza@hey.com>
2025-03-29 19:18:15 -0400

    update lsp for v 0.11

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -1,27 +1,114 @@
 local configs = require('lspconfig.configs')
-local util = require('lspconfig.util')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- JSON language server setup
-require'lspconfig'.jsonls.setup {
-  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/vscode-json-languageserver", "--stdio" },
-  filetypes = { 'json' },
-  root_dir = require'lspconfig'.util.root_pattern(".git", vim.fn.getcwd()),
-  init_options = {
-    provideFormatter = true,
+vim.lsp.config.tailwindcss = {
+  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/tailwindcss-language-server", "--stdio" },
+  filetypes = {
+    -- html
+    'aspnetcorerazor',
+    'astro',
+    'astro-markdown',
+    'blade',
+    'clojure',
+    'django-html',
+    'htmldjango',
+    'edge',
+    'eelixir', -- vim ft
+    'elixir',
+    'ejs',
+    'erb',
+    'eruby', -- vim ft
+    'gohtml',
+    'gohtmltmpl',
+    'haml',
+    'handlebars',
+    'hbs',
+    'html',
+    'htmlangular',
+    'html-eex',
+    'heex',
+    'jade',
+    'leaf',
+    'liquid',
+    'markdown',
+    'mdx',
+    'mustache',
+    'njk',
+    'nunjucks',
+    'php',
+    'razor',
+    'slim',
+    'twig',
+    -- css
+    'css',
+    'less',
+    'postcss',
+    'sass',
+    'scss',
+    'stylus',
+    'sugarss',
+    -- js
+    'javascript',
+    'javascriptreact',
+    'reason',
+    'rescript',
+    'typescript',
+    'typescriptreact',
+    -- mixed
+    'vue',
+    'svelte',
+    'templ',
   },
-}
-
--- YAML language server setup
-require'lspconfig'.yamlls.setup {
-  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/yaml-language-server", "--stdio" }
-}
-require'lspconfig'.sourcekit.setup {}
-
--- Tailwind language server setup
-require'lspconfig'.tailwindcss.setup {
-  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/tailwindcss-language-server", "--stdio" }
+  settings = {
+    tailwindCSS = {
+      validate = true,
+      lint = {
+        cssConflict = 'warning',
+        invalidApply = 'error',
+        invalidScreen = 'error',
+        invalidVariant = 'error',
+        invalidConfigPath = 'error',
+        invalidTailwindDirective = 'error',
+        recommendedVariantOrder = 'warning',
+      },
+      classAttributes = {
+        'class',
+        'className',
+        'class:list',
+        'classList',
+        'ngClass',
+      },
+      includeLanguages = {
+        eelixir = 'html-eex',
+        eruby = 'erb',
+        templ = 'html',
+        htmlangular = 'html',
+      },
+    },
+  },
+  on_new_config = function(new_config)
+    if not new_config.settings then
+      new_config.settings = {}
+    end
+    if not new_config.settings.editor then
+      new_config.settings.editor = {}
+    end
+    if not new_config.settings.editor.tabSize then
+      -- set tab size for hover
+      new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
+    end
+  end,
+  root_markers = {".git",
+    'tailwind.config.js',
+    'tailwind.config.cjs',
+    'tailwind.config.mjs',
+    'tailwind.config.ts',
+    'postcss.config.js',
+    'postcss.config.cjs',
+    'postcss.config.mjs',
+    'postcss.config.ts',
+  },
 }
 
 -- Rust language server setup
@@ -29,21 +116,63 @@ require'lspconfig'.rust_analyzer.setup({
   capabilities = capabilities,
 })
 
-require'lspconfig'.rubocop.setup({
+vim.lsp.config.sourcekit = {
+  cmd = { 'sourcekit-lsp' },
+  filetypes = { 'swift', 'objc', 'objcpp', 'c', 'cpp' },
+  root_markers = {".git", "buildServer.json", '*.xcodeproj', '*.xcworkspace', 'compile_commands.json', 'Package.swift'},
+  get_language_id = function(_, ftype)
+    local t = { objc = 'objective-c', objcpp = 'objective-cpp' }
+    return t[ftype] or ftype
+  end,
+  capabilities = {
+    textDocument = {
+      diagnostic = {
+        dynamicRegistration = true,
+        relatedDocumentSupport = true,
+      },
+    },
+  },
+}
+
+vim.lsp.config.yamlls = {
+  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/yaml-language-server", "--stdio" },
+  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab' },
+  root_markers = {".git"},
+  single_file_support = true,
+  settings = {
+    redhat = { telemetry = { enabled = false } },
+  },
+}
+
+vim.lsp.config.jsonls = {
+  cmd = { "/Users/nick/.config/nvim/language-servers/node_modules/.bin/vscode-json-languageserver", "--stdio" },
+  filetypes = { 'json', 'jsonc' },
+  init_options = {
+    provideFormatter = true,
+  },
+  root_markers = {".git"},
+  single_file_support = true,
+}
+
+vim.lsp.config.rubocop = {
+  cmd = { 'bin/rubocop', '--lsp' },
+  filetypes = { 'ruby' },
+  root_markers = {".git", "Gemfile", "Gemfile.lock"},
   init_options = {
     safeAutocorrect = false,
   }
-})
-require'lspconfig'.ttags.setup({})
+}
 
-if not configs.ruby_locals then
-  configs.ruby_locals = {
-    default_config = {
-      cmd = { "/Users/nick/.config/nvim/language-servers/ruby_locals" },
-      filetypes = {"ruby", "eruby"},
-      root_dir = util.root_pattern('.git')
-    },
-  }
-end
+vim.lsp.config.ttags = {
+  cmd = { 'ttags', 'lsp' },
+  filetypes = { 'ruby', 'rust', 'javascript', 'haskell' },
+  root_markers = {".git"},
+}
 
-require'lspconfig'.ruby_locals.setup({})
+vim.lsp.config.ruby_locals = {
+  cmd = { "/Users/nick/.config/nvim/language-servers/ruby_locals" },
+  filetypes = {"ruby", "eruby"},
+  root_markers = {"Gemfile", "Gemfile.lock"},
+}
+
+vim.lsp.enable({'ruby_locals', 'ttags', 'rubocop', 'jsonls', 'yamlls', 'sourcekit', 'tailwindcss'})
