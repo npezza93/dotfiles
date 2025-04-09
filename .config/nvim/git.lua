8b9d2773bbda74de0913132e42886a5a9b71d71c
Nick Pezza <pezza@hey.com>
2025-04-08 20:52:17 -0400

    fix gitsigns

--- .config/nvim/git.lua
+++ .config/nvim/git.lua
@@ -1,23 +1,13 @@
 require('gitsigns').setup {
   signs = {
-    add          = {text = '│'},
-    change       = {text = '│'},
-    delete       = {text = '_'},
+    add          = { text = '│'},
+    change       = { text = '│'},
+    delete       = { text = '_'},
     topdelete    = { text = '‾'},
-    changedelete = {text = '│'},
-    untracked    = {text = '│'},
+    changedelete = { text = '│'},
+    untracked    = { text = '│'},
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl = false,
+  linehl = false,
 }
-
-vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitSignsAdd' })
-vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAddNr' })
-vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitSignsAddLn' })
-
-vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GitSignsChange' })
-vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChangeNr' })
-vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'GitSignsChangeLn' })
-
-vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete' })
-vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDeleteNr' })
-vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'GitSignsDeleteLn' })
