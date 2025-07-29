a3e890f82fe45a94aafba36505ef1d89dd37a9b9
Nick Pezza <pezza@hey.com>
2025-07-28 20:38:59 -0400

    Gix up gitsigns

--- .config/nvim/git.lua
+++ .config/nvim/git.lua
@@ -1,3 +1,5 @@
+local cp = require("catppuccin.palettes").get_palette()
+
 require('gitsigns').setup {
   signs = {
     add          = { text = '│'},
@@ -11,3 +13,7 @@ require('gitsigns').setup {
   numhl = false,
   linehl = false,
 }
+
+vim.api.nvim_set_hl(0, 'GitSignsAdd',    { fg = cp.green, bg = 'none' })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = cp.yellow, bg = 'none' })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = cp.red, bg = 'none' })
